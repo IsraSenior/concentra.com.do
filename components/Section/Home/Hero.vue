601e22a1f81c6c55994742b1f531e4fd2b248e11
Israel Senior <israsenior.dev@gmail.com>
2025-03-21 13:09:15 -0500

    _logos

--- components/Section/Home/Hero.vue
+++ components/Section/Home/Hero.vue
@@ -65,26 +65,26 @@
             </div>
         </div>
 
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto -mt-20 max-w-7xl px-6 lg:px-8">
             <div
                 class="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg"
+                <img class="col-span-2 w-full object-contain lg:col-span-1"
+                    src="https://concentra.com.do/images/mepyd.jpg"
                     alt="Transistor" width="158" height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform"
+                <img class="col-span-2 w-full object-contain lg:col-span-1"
+                    src="https://concentra.com.do/images/clientes/36.jpg" alt="Reform"
                     width="158" height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple"
+                <img class="col-span-2 w-full object-contain lg:col-span-1"
+                    src="https://concentra.com.do/images/clientes/46.jpg" alt="Tuple"
                     width="158" height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-                    src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal"
+                <img class="col-span-2 w-full object-contain sm:col-start-2 lg:col-span-1"
+                    src="https://concentra.com.do/images/clientes/21.jpg" alt="SavvyCal"
                     width="158" height="48">
-                <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-                    src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic"
+                <img class="col-span-2 col-start-2 w-full object-contain sm:col-start-auto lg:col-span-1"
+                    src="https://concentra.com.do/images/clientes/5.jpg" alt="Statamic"
                     width="158" height="48">
             </div>
-            <div class="mt-16 flex justify-center">
+            <div class="-mt-8 flex justify-center">
                 <p
                     class="relative rounded-full bg-primary-50 px-4 py-1.5 text-sm/6 text-primary-800 ring-1 ring-primary-900/5 ring-inset">
                     <span class="hidden md:inline">Más de 75 empresas utilizan nuestras herramientas para mejorar su
