2a5b52609adf0a4abdd7e203b93f9559d0a6e94b
Israel Senior <israsenior.dev@gmail.com>
2025-04-15 15:05:45 -0500

    index4

---
+++ pages/index4.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <LazySectionHomeHero4 />
+
+        <LazySectionHomeClients />
+
+        <LazySectionHomeAbout />
+
+        <LazySectionHomeStats />
+
+        <LazySectionHomeCTA />
+
+        <LazySectionHomeSolutions />
+
+        <LazySectionHomeTeam />
+
+        <LazySectionHomeTestimonials />
+    </div>
+</template>
+
+<script setup>
+</script>
